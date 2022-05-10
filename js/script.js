324c7ec58f64d8dce4d7c4cd3a0371c6721f39f3
Stas <lu-kaz@yandex.ru>
2022-05-10 08:48:11 +0400

    change index.html

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
-console.log(1);
+"use strict";
+let storeName = 'Apple';
+let storeDescription = {
+    budget: 10000,
+    emplyees:['Ivanov', 'Petrov', 'Sidorov'],
+    products: {
+        iphone12: 120000,
+        iphone13: 130000
+    },
+    open: open
+}
